1d6ba0d322a31507da2c30e76dcfd2b4a91b0f73
octave LE TULLIER <octave.letullier@telecom-paris.fr>
2023-02-03 15:51:00 +0100

    Add links and text

---
+++ src/assets/texts.js
@@ -0,0 +1,9 @@
+export const INFORMATION = {
+    title: 'What is DatArt',
+    intro: `This project made in Vue.js aimed to extract pixel colors from the uploaded video and re-sort them in order to build artistical shapes called circle, 
+            lines, and rose. You can adjust the color threshold depending on your video colors. Moreover you can tune every shape and download them at the end or 
+            during the process.`,
+    about: 'About the creator:',
+    myself: `I am Octave Le Tullier, a double-degree student in Computer Science at KTH & Telecom Paris (2022). I enjoy doing sports and creating websites in my free time. 
+    As data visualization and arts has always piqued my interests, I started developing this project! I hope you'll enjoy it and save lots of your memories in art-shapes! :)`
+}
--- src/components/elements/Modal.vue
+++ src/components/elements/Modal.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import IconTrash from '../icons/IconTrash.vue';
+import IconX from '../icons/IconX.vue';
+import IconLinkedIn from '../icons/IconLinkedIn.vue';
+import IconGitHub from '../icons/IconGitHub.vue';
+import { INFORMATION } from '../../assets/texts'
 
 const props = defineProps<{
     isShown: boolean,
@@ -20,8 +22,18 @@ const toggleModal = () => {
             <div class="greyBackground" @click="toggleModal">
             </div>
             <div class="information" @click="(e: Event) => e.preventDefault()">
-                dededdedde
-                <IconTrash/>
+                <h1>{{ INFORMATION.title }}<img src="/datArtLogo.svg"/></h1>
+                <p class="intro"><img src="/circle.png"/>{{ INFORMATION.intro }}</p>
+                <br/>
+                <p>
+                    <b>{{ INFORMATION.about }}</b>
+                    {{ INFORMATION.myself }}
+                </p>
+                <div class="links">
+                    <a href="https://www.linkedin.com/in/octave-le-tullier" target="_blank"><IconLinkedIn/></a>
+                    <a href="https://github.com/OctaveLT" target="_blank"><IconGitHub/></a>
+                </div>
+                <IconX class="closeIcon" @click="toggleModal"/>
             </div>
         </div>
     </transition>
@@ -41,16 +53,71 @@ const toggleModal = () => {
 }
 
 .modal > .greyBackground {
-    background-color: rgba(197, 197, 197, 0.3);
+    background-color: rgba(197, 197, 197, 0.5);
     position: absolute;
     width: 100%;
     height: 100%;
 }
 
 .modal > .information {
-    padding: 1em;
+    max-width: 70%;
+    padding: 1.8em;
     background-color: #CC998D;
     border-radius: 1em;
+}
+
+.modal > .information > h1 {
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 0.5em;
+}
+
+.modal > .information > h1 > img {
+    margin-left: 0.2em;
+    height: 1em;
+    width: 1em;
+}
+
+.modal > .information > .intro {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.modal > .information > .intro > img {
+    height: 10em;
+    width: 10em;
+    margin-right: 1em;
+}
+
+.modal > .information b {
+    font-weight: bold;
+}
+
+.modal > .information > .links {
+    padding-top: 1.1em;
+    display: flex;
+    justify-content: center;
+}
+
+.modal > .information > .links > a {
+    margin-right: 1em;
+}
+
+.modal > .information > .links > a:hover {
+    transform: scale(1.1);
+}
+
+.modal > .information > .closeIcon {
+    position: absolute;
+    top: 0.5em;
+    right: 0.5em;
+}
+
+.modal > .information > .closeIcon:hover {
+    transform: scale(1.3);
 }
 
 .fade-enter-active, .fade-leave-active {
